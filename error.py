1c184b383067a63d1b1b7674c41143ffb56fed25
Olimpia <soulspime@gmail.com>
2024-11-20 03:35:41 +0100

    refactor error handling

--- error.py
+++ error.py
@@ -1,39 +1,20 @@
 import discord
+import language
 
-error_language = {
-    "noVoice": {
-        "pl": "Musisz być na kanale głosowym, żeby to zrobić.",
-        "en": "You have to be in a voice channel to do that."
-    },
-    "wrongVoice": {
-        "pl": "Musisz być na tym samym kanale głosowym co gra.",
-        "en": "You must be in the same voice channel as the game."
-    },
-    "gameOngoing": {
-        "pl": "Na tym kanale głosowym jest już aktywna gra.",
-        "en": "There is already a game ongoing in this voice channel."
-    },
-    "noGame": {
-        "pl": "Na tym kanale głosowym nie ma aktywnej gry.",
-        "en": "There is no game ongoing in this voice channel right now."
-    },
-    "notHost": {
-        "pl": "Tylko host może to zrobić.",
-        "en": "Only the host can do that."
-    }
-}
+async def send(interaction: discord.Interaction, error: str, languageCode: str):
+    return await interaction.response.send_message(str(language.getModule("errors", languageCode)[error]), ephemeral=True)
 
-async def noVoice(interaction: discord.Interaction, language: str):
-    return await interaction.response.send_message(error_language["noVoice"][language], ephemeral=True)
+async def noVoice(interaction: discord.Interaction, languageCode: str):
+    return await send(interaction, "noVoice", languageCode)
 
-async def wrongVoice(interaction: discord.Interaction, language: str):
-    return await interaction.response.send_message(error_language["wrongVoice"][language], ephemeral=True)
+async def wrongVoice(interaction: discord.Interaction, languageCode: str):
+    return await send(interaction, "wrongVoice", languageCode)
 
-async def gameOngoing(interaction: discord.Interaction, language: str):
-    return await interaction.response.send_message(error_language["gameOngoing"][language], ephemeral=True)
+async def gameOngoing(interaction: discord.Interaction, languageCode: str):
+    return await send(interaction, "gameOngoing", languageCode)
 
-async def noGame(interaction: discord.Interaction, language: str):
-    return await interaction.response.send_message(error_language["noGame"][language], ephemeral=True)
+async def noGame(interaction: discord.Interaction, languageCode: str):
+    return await send(interaction, "noGame", languageCode)
 
-async def notHost(interaction: discord.Interaction, language: str):
-    return await interaction.response.send_message(error_language["notHost"][language], ephemeral=True)
+async def notHost(interaction: discord.Interaction, languageCode: str):
+    return await send(interaction, "notHost", languageCode)
